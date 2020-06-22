f0f407ed52d2774310ba0c399c3a597be45e9ea0
m-dalbosco <56672981+m-dalbosco@users.noreply.github.com>
2020-06-22 18:20:36 +0200

    Created new FEMSoE module for Periodic BC

---
+++ SourceCode/FEM/Modules/ModMultiscalePeriodicFEMSoE.f90
@@ -0,0 +1,112 @@
+module ModMultiscalePeriodicFEMSoE
+
+    use ModNonLinearSystemOfEquations
+    use ModAnalysis
+    use BoundaryConditions
+    use ElementLibrary
+    use GlobalSparseMatrix
+
+    implicit none
+
+    type , extends(ClassNonLinearSystemOfEquations) :: ClassMultiscalePeriodicFEMSoE
+
+        real(8), dimension(:), allocatable                   :: Fint , Fext , UBar
+        real(8)                                              :: Time
+        integer, dimension(:), pointer                       :: DispDOF
+
+        integer, dimension(:), allocatable                   :: PrescDispSparseMapZERO
+        integer, dimension(:), allocatable                   :: PrescDispSparseMapONE
+        integer, dimension(:), allocatable                   :: FixedSupportSparseMapZERO
+        integer, dimension(:), allocatable                   :: FixedSupportSparseMapONE
+        
+        type  (ClassElementsWrapper), dimension(:), pointer  :: ElementList
+        type  (ClassNodes), dimension(:), pointer            :: GlobalNodesList
+        type  (ClassAnalysis)                                :: AnalysisSettings
+        class (ClassBoundaryConditions), pointer             :: BC
+        type  (ClassGlobalSparseMatrix), pointer             :: Kg
+        !type  (ClassGlobalSparseMatrix), pointer             :: TMat
+
+
+    contains
+
+        procedure :: EvaluateSystem => EvaluateR
+        procedure :: EvaluateGradientSparse => EvaluateKt
+        procedure :: PostUpdate => FEMUpdateMesh
+
+    end type
+
+    contains
+
+!--------------------------------------------------------------------------------------------------
+    subroutine EvaluateR(this,X,R)
+
+        use Interfaces
+        class(ClassMultiscalePeriodicFEMSoE) :: this
+        real(8),dimension(:) :: X,R
+
+            ! Update stress and internal variables
+            call SolveConstitutiveModel( this%ElementList , this%AnalysisSettings , this%Time, X, this%Status)
+
+            ! Constitutive Model Failed. Used for Cut Back Strategy
+            if (this%Status%Error ) then
+                return
+            endif
+
+            ! Internal Force
+            call InternalForce(this%ElementList , this%AnalysisSettings , this%Fint, this%Status)
+
+            ! det(Jacobian Matrix)<=0 .Used for Cut Back Strategy
+            if (this%Status%Error ) then
+                return
+            endif
+
+            ! Residual
+            R = this%Fint - this%Fext
+
+    end subroutine
+
+!--------------------------------------------------------------------------------------------------
+
+    subroutine EvaluateKt(this,X,R,G)
+
+        use Interfaces
+        use MathRoutines
+        class(ClassMultiscalePeriodicFEMSoE)        :: this
+        class (ClassGlobalSparseMatrix), pointer :: G
+        real(8),dimension(:) :: X , R
+        real(8) :: norma
+        integer :: nDOF
+
+        call this%AnalysisSettings%GetTotalNumberOfDOF (this%GlobalNodesList, nDOF)
+
+        call TangentStiffnessMatrix(this%AnalysisSettings , this%ElementList , nDOF, this%Kg )
+
+        ! As CC de deslocamento prescrito estão sendo aplicadas no sistema Kx=R e não em Kx=-R!!!
+        R = -R
+        !call this%BC%ApplyBoundaryConditions(  this%Kg , R , this%DispDOF, this%Ubar , X   )
+        call this%BC%ApplyBoundaryConditionsNEW(  this%Kg , R , this%DispDOF, this%Ubar , X, this%PrescDispSparseMapZERO, this%PrescDispSparseMapONE, this%FixedSupportSparseMapZERO, this%FixedSupportSparseMapONE )
+        R = -R
+
+        G => this%Kg
+
+    end subroutine
+
+!--------------------------------------------------------------------------------------------------
+
+    subroutine FEMUpdateMesh(this,X)
+        use Interfaces
+        class(ClassMultiscalePeriodicFEMSoE) :: this
+        real(8),dimension(:)::X
+
+        if (this%AnalysisSettings%NLAnalysis == .true.) then
+            call UpdateMeshCoordinates(this%GlobalNodesList,this%AnalysisSettings,X)
+        endif
+
+    end subroutine
+
+
+
+
+
+end module
+
